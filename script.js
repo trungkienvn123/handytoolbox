48b65690042ecdb70b8379d1fc2c6b996d352eb5
trungkienvn123 <142674399+trungkienvn123@users.noreply.github.com>
2023-09-01 17:03:55 +0700

    new clock function

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const myInterval = setInterval(myTimer, 1000);
 function myTimer() {
   const date = new Date();
-  document.getElementById("clock").innerHTML = date.toLocaleTimeString('en-US');
   document.getElementById("subClock").innerHTML = date.toLocaleTimeString('en-US');
 }
-function myFunction(){
-  document.getElementById("clock-part").style.display = "none";    }
+function closeFunction(){
+  document.querySelector(".sub-clock").style.display = "none";
+  document.querySelector(".close-icon").style.display = "none";    }
 
-
-function myFunction2(){
-  document.getElementById("clock").style.position="fixed"
-  document.getElementById("clock").style.top="0"
-}
 function clockAppear(){
-  document.querySelector(".sub-clock").style.display="block"
+  document.querySelector(".sub-clock").style.display="block";
+  document.querySelector(".close-icon").style.display="block";
 }
+function updateClock(){
+  let now = new Date();
+  let second = now.getSeconds();
+  let minute = now.getMinutes();
+  let hour = now.getHours();
+  document.getElementById("second").style.transform = `rotate(${90 + second * 6 }deg)`;
+  document.getElementById("minute").style.transform = `rotate(${90 + minute * 6 }deg)`;
+  document.getElementById("hour").style.transform = `rotate(${90 + hour * 30 }deg)`;
+}
+setInterval(updateClock,1000);
